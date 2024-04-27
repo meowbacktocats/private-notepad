220edaafc422e9cf36a4cac555424dca8993b566
Kallum <168292758+meowbacktocats@users.noreply.github.com>
2024-04-27 18:09:42 +0100

    Add non-functional login route.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
     return '<p>Hello, world!</p>'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        ... # Validate details.
+    return '' # Render Login template.
 
 @app.get('/note/<int:note_id>')
 def notes(note_id: int):
